2a102efe5a1afe7e957069e1bb367776c26e3a09
moataz osama <moataz.osama@hotmail.com>
2020-03-19 17:27:12 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 // var chocolateBars=["snickers", "hundred grand", "kitkat","skittles"]
 
 // function addElementToBeginningOfArray(array,marsa){
-  function addElementToBeginningOfArray (array,element)
+  function addElementToBeginningOfArray (array,1)
   {array=["foo"]
 array.unshift()
 return array
@@ -9,3 +9,4 @@ return array
 // return addElementToBeginningOfArray()
 
 // function destructivelyAddElementToBeginningOfArray
+
